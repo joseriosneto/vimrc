dd5f7db2945f1f1b0ff572519f101a4278617551
joseriosneto <joseriosneto@gmail.com>
2015-08-28 21:49:36 +0000

    Added install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+          curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Fugitive
+mkdir -p ~/.vim/bundle && \
+          git clone https://github.com/tpope/vim-fugitive ~/.vim/bundle/vim-fugitive
