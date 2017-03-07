241cec0ca3ee79838a83f9605d25307e2f621f65
Jose Rios <joseriosneto@arista.com>
2017-03-07 20:40:15 +0000

    Added surround.vim
    It allows to add/replace/delete surrounding chars on words
    or visual selections.

--- install.sh
+++ install.sh
@@ -9,3 +9,6 @@ git clone https://github.com/tpope/vim-fugitive ~/.vim/bundle/vim-fugitive
 
 # TagBar
 git clone https://github.com/majutsushi/tagbar ~/.vim/bundle/tagbar
+
+# Surround.vim
+git clone git://github.com/tpope/vim-surround ~/.vim/bundle/vim-surround
